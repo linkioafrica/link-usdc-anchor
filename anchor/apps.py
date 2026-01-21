08cd94196bae95a838d4fcddd149069f3ae0ee3e
Tomisin Leshi <tom-lesh@outlook.com>
2026-01-21 17:31:57 +0100

    Initial project setup with Django and Polaris integration
    Add Django project structure for the 'anchor' app, including settings, ASGI/WSGI configs, and Polaris integration. Include static assets, initial TOML integration, and supporting scripts for frontend functionality. Add .gitignore and requirements.txt for environment and dependency management.

---
+++ anchor/apps.py
@@ -0,0 +1,21 @@
+from django.apps import AppConfig
+
+class AnchorConfig(AppConfig):
+    name = "anchor"
+
+    def ready(self):
+        from polaris.integrations import register_integrations
+
+        from .integrations import (
+            toml_contents,
+            # AnchorDeposit,
+            # AnchorWithdraw,
+            # AnchorRails,
+        )
+
+        register_integrations(
+            toml = toml_contents,
+            # deposit=AnchorDeposit(),
+            # withdrawal=AnchorWithdraw(),
+            # rails=AnchorRails(),
+        )
